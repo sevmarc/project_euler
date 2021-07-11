fb15fddeefc54b457a29c71a7c2bb6e48548fec3
sevmarc <marton.sevella@gmail.com>
2021-07-11 21:45:53 +0200

    #04 fixed, simplified, docs

--- 04.py
+++ 04.py
@@ -1,20 +1,14 @@
-def check_pal(n: int):
-    if str(n) == (str(n))[::-1]:
-        return True
-    else:
-        return False
+""" Largest palindrome product
+A palindromic number reads the same both ways. The largest palindrome 
+made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
 """
-print(check_pal(1111))
-print(check_pal(1211))
-print(check_pal(1221111))
-print(check_pal(113311))
-"""
+
+from function_collection import is_palindrome
 
 def pal():
-    for i in range(999,900,-1):
-        for j in range(999,900,-1):
-            if check_pal(i*j):
-                return i*j
-    return None
-
-print(pal())
+    return max(i*j for i in range(100,999) for j in range(100,999) if is_palindrome(i*j))
+    
+if __name__ == '__main__':
+    print(pal())
