99552ef89c86b840d8e8dc70b370d2b40e01956e
sevmarc <marton.sevella@gmail.com>
2021-07-12 20:55:32 +0200

    #06 simplified, docs

--- 06.py
+++ 06.py
@@ -1,23 +1,36 @@
-def sum_square(n: list):
+""" Sum square difference
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 +...+ 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+(1 + 2 +...+ 10)^2 = 3025
+Hence the difference between the sum of the squares of the first 
+ten natural numbers and the square of the sum is 3025-385 = 2640.
+
+Find the difference between the sum of the squares of the first 
+one hundred natural numbers and the square of the sum.
+"""
+
+def sum_square(n: list[int]) -> int:
     sum = 0
     for i in n:
         sum += i * i
     return sum
 
-def square_sum(m: list):
+def square_sum(m: list[int]) -> int:
     sum = 0
     for i in m:
         sum += i
     return sum * sum
+
 """
 example_l = range(1,11)
 print(sum_square(example_l))
 print(square_sum(example_l))
 print(square_sum(example_l) - sum_square(example_l))
 """
-task_l = range(1,101)
-ss1 = sum_square(task_l)
-print(ss1)
-ss2 = square_sum(task_l)
-print(ss2)
-print(ss2 - ss1)
+
+if __name__ == '__main__':        
+    numlist = range(1,101)
+    sum_of_squares = sum_square(numlist)
+    square_of_sums = square_sum(numlist)
+    print(square_of_sums - sum_of_squares)
