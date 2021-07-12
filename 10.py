0078a57756814dadbc2045b6bd7ddf75b9159681
sevmarc <marton.sevella@gmail.com>
2021-07-12 21:34:27 +0200

    #10 __main__ fixed, timer wrapper, docs added

--- 10.py
+++ 10.py
@@ -1,24 +1,15 @@
-from math import sqrt
+""" Summation of primes
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-def is_prime(x):
-    if x == 1:
-        return False
-    check = 0
-    for i in range(2, int(sqrt(x)) + 1):
-        if x % i == 0:
-            check += 1
-            if check > 1:
-                return False
-    if check == 0:
-        return True
-    else:
-        return False
+Find the sum of all the primes below two million.
+"""
+
+from function_collection import is_prime, timer_wrapper
 
 
-max = 2000000
-sum = 0
-for i in range(1,max + 1):
-    if is_prime(i):
-        sum += i
+def calc10(max_: int) -> int:
+    return sum([i for i in range(1, max_ + 1) if is_prime(i)])
 
-print(sum)
+if __name__ == '__main__':
+    print(timer_wrapper(calc10, 2000000))
+    
