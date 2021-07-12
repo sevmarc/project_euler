6e35716a65e0138585741a58e621d53c4d3300c2
sevmarc <marton.sevella@gmail.com>
2021-07-12 21:23:33 +0200

    #09 __main__ fixed, exit added, docs added

--- 09.py
+++ 09.py
@@ -1,17 +1,29 @@
-# a + b + c = 1000
-# a2 + b2 = c2
+""" Special Pythagorean triplet
+A Pythagorean triplet is a set of three natural numbers, 
+a < b < c, for which,
+a^2 + b^2 = c^2
 
-def check_pyt(a, b, c):
-    return a*a + b*b == c*c
-# test
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which 
+a + b + c = 1000.
+Find the product abc.
 """
+
+def check_pyt(a: int, b: int, c: int) -> bool:
+    return a*a + b*b == c*c
+
+"""
+# test
 print(check_pyt(1,2,3))
 print(check_pyt(3,4,5))
 """
 
-max = 998
-for i in range(1,max):
-    for j in range(1,max - i):
-        z = 1000 - i - j
-        if check_pyt(i,j,z):
-            print(i, j, z, i*j*z)
+if __name__ == '__main__':
+    max_ = 998
+    for a in range(1, max_):
+        for b in range(1, max_ - a):
+            c = 1000 - a - b
+            if check_pyt(a,b,c):
+                print(a, b, c, a*b*c)
+                exit()
