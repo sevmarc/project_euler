e7d6e047d8807aab0ede4c48cc37cf2fc84b864f
sevmarc <marton.sevella@gmail.com>
2021-07-13 21:23:54 +0200

    #14 extended, still kinda slow at ~35 sec

--- 14.py
+++ 14.py
@@ -1,4 +1,29 @@
-def collatz_len(n, counter=0):
+""" Longest Collatz sequence
+The following iterative sequence is defined 
+for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, 
+we generate the following sequence:
+
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and 
+finishing at 1) contains 10 terms. Although it has not 
+been proved yet (Collatz Problem), it is thought that 
+all starting numbers finish at 1.
+
+Which starting number, under one million, 
+produces the longest chain?
+
+NOTE: Once the chain starts the terms are 
+allowed to go above one million.
+"""
+
+from function_collection import timer_wrapper
+
+def collatz_len(n: int, counter:int=0) -> int:
     counter += 1
     if (n == 1):
         return counter
@@ -7,15 +32,17 @@ def collatz_len(n, counter=0):
     else:
         return collatz_len(3 * n + 1, counter)
 
-full = 1000000
-max = 0
-max_place = 0
-for i in range(1, full):
-    cur = collatz_len(i)
-    if cur > max:
-        max = cur
-        max_place = i
+def calc14(full=1000000):
+    max_ = 0
+    max_place = 0
+    for i in range(1, full):
+        cur = collatz_len(i)
+        if cur > max_:
+            max_ = cur
+            max_place = i
+    print(max_place, max_)
 
-print(max_place, max)
+    # print(ans_path[0], ans)
 
-# print(ans_path[0], ans)
+if __name__ == '__main__':
+    timer_wrapper(calc14)
