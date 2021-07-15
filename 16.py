b665b4f856d1ad7dcaa84b3d0cb3ed8bbe42dff1
sevmarc <marton.sevella@gmail.com>
2021-07-15 19:18:11 +0200

    #16 update, docs

--- 16.py
+++ 16.py
@@ -1,11 +1,18 @@
+""" Power digit sum
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+"""
+
 from math import sqrt
 
-def powdig(x,y):
+def powdig(x: int,y: int) -> int:
     val = pow(x,y)
     sum = 0
     for w in str(val):
         sum += int(w)
     return sum
 
-print(powdig(2, 15))
-print(powdig(2, 1000))
+if __name__ == '__main__':
+    # print(powdig(2, 15))
+    print(powdig(2, 1000))
