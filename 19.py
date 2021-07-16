7fae3e5e27b1b5b07ac599317579c38f46bc1ed1
sevmarc <marton.sevella@gmail.com>
2021-07-17 00:07:32 +0200

    #19 extended with docs, import for timer_wrapper function

--- 19.py
+++ 19.py
@@ -1,13 +1,30 @@
-# Counting Sundays
+""" Counting Sundays
+You are given the following information, but you may prefer 
+to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but 
+not on a century unless it is divisible by 400.
+
+How many Sundays fell on the first of the month during the 
+twentieth century (1 Jan 1901 to 31 Dec 2000)?
+"""
+
 import datetime
-import time
+from function_collection import timer_wrapper
 
-def loop_through_days():
+def loop_through_days() -> int:
     start_date = datetime.date(1901,1,1)
-    end_date = datetime.date(2000,12,31)
-    delta = datetime.timedelta(days=1)
+    end_date:datetime.date = datetime.date(2000,12,31)
+    delta:datetime.date = datetime.timedelta(days=1)
 
-    counter = 0
+    counter: int = 0
     while start_date <= end_date:
         # print(start_date)
         if start_date.weekday() == 6 and start_date.day == 1:
@@ -15,9 +32,5 @@ def loop_through_days():
         start_date += delta
     return counter
 
-start_time = time.time()
-
-result = loop_through_days()
-print(result)
-
-print(time.time() - start_time)
+if __name__ == '__main__':
+    print(timer_wrapper(loop_through_days))
