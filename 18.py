a9ce9cf4c0777ba703e9f93c4aac60ab1e0eead3
sevmarc <marton.sevella@gmail.com>
2021-07-16 23:53:20 +0200

    #18 docs added, __main__ used for running script

--- 18.py
+++ 18.py
@@ -1,5 +1,36 @@
-# Maximum path sum I
-import time
+""" Maximum path sum I 
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+"""
+
+from function_collection import timer_wrapper
+
 
 test_triangle = [
           [3],
@@ -25,7 +56,7 @@ triangle = [
     [63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
     [ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]]
 
-
+# unused functions
 def str_to_int(tri):
     rows = []
     for row in tri:
@@ -35,7 +66,7 @@ def str_to_int(tri):
         rows.append(row2)
     return rows
 
-def read_triangle_from_file(f):
+def read_triangle_from_file(f: str):
     return str_to_int([(line.replace('\n', '').split(' ')) for line in open(f, 'r')])
 
 def naive_path_finder(tri):
@@ -66,10 +97,5 @@ def find_best_path(tri):
     print('Found {} in {} iterations'.format(tri[0][0],counter))
 
 
-start_time = time.time()
-
-# print(triangle)
-# print(read_triangle_from_file("18.txt"))
-find_best_path(triangle)
-
-print(time.time() - start_time)
+if __name__ == '__main__':
+    timer_wrapper(find_best_path, triangle)
