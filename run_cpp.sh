ac36b36e138b06a997465db970472b8682f324a2
sevmarc <marton.sevella@gmail.com>
2021-08-06 07:40:41 +0200

    updated run_cpp.sh script to print elapsed time aswell

--- run_cpp.sh
+++ run_cpp.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+start_time="$(date -u +%s.%N)"  # seconds.nanoseconds
+
 # && -> the next command only runs if previous succeeded
 g++ $1 -o run_temp && ./run_temp && rm run_temp
+
+end_time="$(date -u +%s.%N)"
+
+elapsed="$(bc <<<"$end_time-$start_time")"
+
+echo "Total of $elapsed seconds elapsed for process $1"
