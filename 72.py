11c9cc2b5b4a08f4a1f9b1c50dd0338ead0cb489
sevmarc <marton.sevella@gmail.com>
2021-11-14 10:32:55 +0100

    Solution (under 60 sec) using builtin Euler's totient function

--- 72.py
+++ 72.py
@@ -15,88 +15,49 @@ fractions for d ≤ 1,000,000?
 """
 
 from function_collection.main import unique_prime_divisors, timer_wrapper
-from itertools import chain, combinations
 from math import gcd as bltin_gcd
-
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
-def mult(iterab):
-    """Multiply all numbers in the iterable and return result"""
-    rv = 1
-    for k in iterab:
-        rv = rv * k
-    return rv
+from math import sqrt
+from sympy import totient
 
 
+def hcf_1(d: int) -> int:
+    """ Returns the number of numbers that have a HCF 
+    of 1 with the input 'd' number """
+    count = 0
+    for i in range(1, d):
+        if bltin_gcd(i, d) == 1:
+            # print("\t", i, d)
+            count += 1
+    return count
 
-def phi(n:int, printer=False) -> int:
-    divs = unique_prime_divisors(n)
-    combs = powerset(divs)
-    combinations = {}
-    for c in combs:
-        if len(c) > 1 and mult(c) < n:
-            if len(c) in combinations.keys():
-                list_at_lenc = combinations[len(c)]
-                list_at_lenc.append(mult(c))
-                combinations.update( { len(c): list_at_lenc })
-            else:
-                combinations.update( {len(c): [mult(c)]} )
-    if printer:
-        print(combinations)
-        print(f'n: {n}\t divs: {divs}')
+def calc_rec(lim_: int) -> int:
+    """ Sums hcf_1s recursively """
+    if lim_ == 1:
+        return 0
+    return hcf_1(lim_) + calc_rec(lim_-1)
     
-    phires = n
+def calc_lin(lim_: int) -> int:
+    """ Sums hcf_1s linearly """
+    sum_ = 0
+    for i in range(lim_ + 1):
+        sum_ += hcf_1(i)
+    return sum_
 
-    for i in divs:
-        if printer:
-            print(f'n: {n}\t n/{i} - 1: {(int(n/i) - 1)}\t -> phires: {phires-(int(n/i) - 1)}')
-        phires -= (int(n/i) - 1)  # -1 because n not counted in phi
-    for lenght, c in combinations.items():
-        if lenght % 2 == 0:
-            for c_iter in c:
-                if printer:
-                    print(f'n: {n}\t n/{c_iter} - 1: {(int(n/c_iter) - 1)}\t -> phires: {phires+(lenght - 1)*(int(n/c_iter) - 1)}')
-                phires += (int(n/c_iter) - 1)
-        else:
-            for c_iter in c:
-                if printer:
-                    print(f'n: {n}\t n/{c_iter} - 1: {(int(n/c_iter) - 1)}\t -> phires: {phires+(lenght - 1)*(int(n/c_iter) - 1)}')
-                phires -= (int(n/c_iter) - 1)
-    if printer:
-        print(f'RETURN: {phires-1}')
-    """
-    counter = 0
-    for i in range(n):
-        if bltin_gcd(n, i) == 1:
-            counter += 1
-    print(f'CHECK: gcd({n},...) = {counter}')
-    """
-    return phires  # -1 because n not counted in phi
+def calc_tot(lim_: int) -> int:
+    """ Sums hcf_1s using Euler's totient function (from sympy) """
+    sum_ = 0
+    for i in range(2, lim_ + 1):
+        sum_ += totient(i)
+    return sum_
 
-    # old solution, was too slow
-    # phi = [i for i in range(1,n) if (bltin_gcd(n, i) == 1)]
-    # return len(phi)
-
-def calculate_72(x: int):
-    counter = 0
-    for d in range(2, x + 1):  # up till x
-        counter += phi(d, False)
-    print('Result: ', x,'\t\t', counter)
-        
 
 testing = False
 
 if __name__ == '__main__':
     if testing:
-        # calculate_72(10)
-        timer_wrapper(calculate_72, 60)
-        # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
-        #   ^   ^   ^   ^    ^     ^     ^     ^    = 8
-        #     ^     ^     ^        ^        ^       = 5
-        # 1/18, 5/18, 7/18, 11/18, 13/18, 17/18
-        
+        for i in range(2,26):
+            print(i, hcf_1(i))
     else:
-        timer_wrapper(calculate_72, 1000000)
+        val = int(1e6)
+        print(timer_wrapper(calc_tot, val))
+
