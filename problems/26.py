1607d1185dfd6ac40358d6d4e075a7089741889a
sevmarc <marton.sevella@gmail.com>
2022-08-14 17:37:00 +0200

    Documentation, formatting

--- problems/26.py
+++ problems/26.py
@@ -1,4 +1,23 @@
-"""
+""" Reciprocal cycles
+A unit fraction contains 1 in the numerator. The decimal representation 
+of the unit fractions with denominators 2 to 10 are given:
+
+1/2	= 	0.5
+1/3	= 	0.(3)
+1/4	= 	0.25
+1/5	= 	0.2
+1/6	= 	0.1(6)
+1/7	= 	0.(142857)
+1/8	= 	0.125
+1/9	= 	0.(1)
+1/10	= 	0.1
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. 
+It can be seen that 1/7 has a 6-digit recurring cycle.
+
+Find the value of d < 1000 for which 1/d contains the longest recurring 
+cycle in its decimal fraction part.
+
+
 https://oeis.org/A051626
 https://oeis.org/A051626/b051626.txt
 """
@@ -6,17 +25,13 @@ https://oeis.org/A051626/b051626.txt
 from function_collection.utils import handle_filepath
 
 
-f_open = open(handle_filepath("inputfiles/26.txt"), "r")
-lines = {}
-for line in f_open:
-    line = line.split()
-    lines.update({line[0]: line[1]})
-# print(lines)
+if __name__ == '__main__':
+    f_open = open(handle_filepath("inputfiles/26.txt"), "r")
+    lines = {line[0]: line[1] for line in f_open}
 
-max = 0
-place = 0
-for j,k in lines.items():
-    if int(k) > max:
-        max = int(k)
-        place = j
-print(place, ': ', max)
+    max = place = 0
+    for j, k in lines.items():
+        if int(k) > max:
+            max = int(k)
+            place = int(j)
+    print(f"{place = }: {max = }")
