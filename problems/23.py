843f89bb68f7dbca9c0053b7a602e540ccde5659
sevmarc <marton.sevella@gmail.com>
2022-08-14 17:18:10 +0200

    Fix formatting, optimization

--- problems/23.py
+++ problems/23.py
@@ -1,20 +1,21 @@
 # Non-abundant sums
-import time
+from function_collection.main import timer_wrapper
+from function_collection.main import proper_divisors_fast
 
-def proper_divisors(x:int):
-    return [i for i in range(1, int(x/2 + 1)) if x % i == 0]
-# print(proper_divisors(28))
 
-def deficient(x):
-    return x > sum(proper_divisors(x))
-def abundant(x):
-    return x < sum(proper_divisors(x))
-def perfect(x):
-    return x == sum(proper_divisors(x))
+def deficient(x: int) -> bool:
+    return x > sum(proper_divisors_fast(x))
 
-limit = 28123
-abundants = [i for i in range(1, limit + 1) if abundant(i)]
-def check_abundant_sum(x, ab=abundants):
+
+def abundant(x: int) -> bool:
+    return x < sum(proper_divisors_fast(x))
+
+
+def perfect(x: int) -> bool:
+    return x == sum(proper_divisors_fast(x))
+
+
+def check_abundant_sum(x, ab: list[int]) -> bool:
     values = [i for i in ab if i < x]
     for v in values:
         if (x - v) in ab:
@@ -22,23 +23,21 @@ def check_abundant_sum(x, ab=abundants):
             return True
     return False
 
-def checker():
-    sum = 0
-    for i in range(1, limit):
-        if not check_abundant_sum(i):
-            sum += i
-    return sum
 
-"""
-# tests
-print(len(abundants))
-print(check_abundant_sum(24))
-print(check_abundant_sum(25))
-print(check_abundant_sum(23))
-"""
+def checker(ab: list[str]) -> int:
+    return sum([i for i in range(1, limit) if not check_abundant_sum(i, ab)])
 
-start_time = time.time()
 
-print(checker())
+if __name__ == '__main__':
+    testing = False
 
-print(time.time() - start_time)
+    limit = 28123
+    abundants = [i for i in range(1, limit + 1) if abundant(i)]
+
+    if testing:
+        print(len(abundants))
+        print(check_abundant_sum(24, abundants))
+        print(check_abundant_sum(25, abundants))
+        print(check_abundant_sum(23, abundants))
+    else:
+        print(timer_wrapper(checker, abundants))
