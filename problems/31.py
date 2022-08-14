eee9c208912301a1f32f2ca4c8733177e5d31f10
sevmarc <marton.sevella@gmail.com>
2022-08-14 18:20:08 +0200

    Added documentation, formatting

--- problems/31.py
+++ problems/31.py
@@ -1,31 +1,26 @@
-import time
+""" Coin sums
+In the United Kingdom the currency is made up of pound (£) and pence (p).
+There are eight coins in general circulation:
 
-#time at the start of the program execution
-start = time.time()
+1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+It is possible to make £2 in the following way:
 
-#counter for couting number of possibilities
-counter = 0
+1x£1 + 1x50p + 2x20p + 1x5p + 1x2p + 3x1p
+How many different ways can £2 be made using any number of coins?
+"""
 
-# number of 100p coins
-for a in range(3):
-	# number of 50p coins
-	for b in range(int(1+(200-100*a)/50)):
-		# number of 20p coins
-		for c in range(int(1+(200-100*a-50*b)/20)):
-			# number of 10p coins
-			for d in range(int(1+(200-100*a-50*b-20*c)/10)):
-			    # number of 5p coins
-				for e in range(int(1+(200-100*a-50*b-20*c-10*d)/5)):
-					# number of 2p coins
-					for f in range(int(1+(200-100*a-50*b-20*c-10*d-5*e)/2)):
-						counter += 1
+from function_collection.main import timer_wrapper
 
-# Total number of ways we can form the 200p
-# Added 1 for 200p case
-print(counter + 1)
 
-#time at the end of program execution
-end = time.time()
+if __name__ == '__main__':
+    counter_ = 1  # counter for couting number of possibilities, 1 for 200p case
 
-#Total time taken for the program execution
-print(end - start)
+    for a in range(3):  # number of 100p coins
+        for b in range(int(1+(200-100*a)/50)):  # number of 50p coins
+            for c in range(int(1+(200-100*a-50*b)/20)):  # number of 20p coins
+                for d in range(int(1+(200-100*a-50*b-20*c)/10)):  # number of 10p coins
+                    for e in range(int(1+(200-100*a-50*b-20*c-10*d)/5)):  # number of 5p coins
+                        # number of 2p coins
+                        for _ in range(int(1+(200-100*a-50*b-20*c-10*d-5*e)/2)):
+                            counter_ += 1
+    print(counter_)
