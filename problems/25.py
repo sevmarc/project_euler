a93ad0ecb43cefed0960741021d94b28c7943dba
sevmarc <marton.sevella@gmail.com>
2022-08-14 17:30:07 +0200

    Add documentation, formatting

--- problems/25.py
+++ problems/25.py
@@ -1,31 +1,26 @@
-from math import pow
+""" 1000-digit Fibonacci-number
+The Fibonacci sequence is defined by the recurrence relation:
 
+Fn = Fn-1 + Fn-2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 """
-def fibo(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    else:
-        return fibo(n-1) + fibo(n-2)
 
-# test
-for i in range(1,15):
-    print(i, ': ', fibo(i))
-
-
-phi = 1.61803398875
-arg = 100
-calc = fibo(arg)
-print(arg, ': ', calc)
-for i in range(1,100000000):
-    # print(i, ': ', fibo(i), fibo(10)*pow(phi, i-10))
-    # print(i, ': ', fibo(10)*pow(phi, i-10))
-    cur = calc*pow(phi, i-arg)
-    if len(str(cur)) == 1000:
-        print(i, ': ', cur)
-        break
-"""
 
 def fibo():
     a = 0
@@ -33,6 +28,7 @@ def fibo():
     while True:
         yield a
         a, b = b, a+b
+
 
 for index, number in enumerate(fibo()):
     if len(str(number)) == 1000:
