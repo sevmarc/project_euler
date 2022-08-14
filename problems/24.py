040c46a2ca14e066a8103a8a579fd24825c1a20c
sevmarc <marton.sevella@gmail.com>
2022-08-14 17:27:43 +0200

    Minor fix, formatting

--- problems/24.py
+++ problems/24.py
@@ -1,16 +1,26 @@
+""" Lexicographic permutations
+A permutation is an ordered arrangement of objects. For example, 
+3124 is one possible permutation of the digits 1, 2, 3 and 4. 
+If all of the permutations are listed numerically or alphabetically, 
+we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+"""
+
 from itertools import permutations
 
-ex_lst = list('012')
-task_lst = list(range(10))
+if __name__ == '__main__':
+    testing = False
 
-ex = list(permutations(ex_lst))
-# print(ex)
-
-# print(task_lst)
-task = list(permutations(task_lst))
-ind = 999999
-result = ''
-for w in (task[ind]):
-    result += str(w)
-print(result)
-# print(task[ind])
+    if testing:
+        ex_lst = list('012')
+        ex = list(permutations(ex_lst))
+        print(ex)
+    else:
+        task_lst = list(range(10))
+        task = list(permutations(task_lst))
+        ind = 999999
+        result = ''.join([str(w) for w in task[ind]])
+        print(f"{int(result) = }")
