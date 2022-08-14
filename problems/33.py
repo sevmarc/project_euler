4c64f7a63dd894a2cd7b1013af3c5d42ef228e89
sevmarc <marton.sevella@gmail.com>
2022-08-14 18:35:53 +0200

    Refactoring, added documentation

--- problems/33.py
+++ problems/33.py
@@ -1,7 +1,21 @@
-def curious_fraction(a: int, b: int):
+""" Digit cancelling fractions
+The fraction 49/98 is a curious fraction, as an inexperienced 
+mathematician in attempting to simplify it may incorrectly believe 
+that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+
+We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+
+There are exactly four non-trivial examples of this type of fraction, 
+less than one in value, and containing two digits in the numerator and denominator.
+
+If the product of these four fractions is given in its lowest common 
+terms, find the value of the denominator.
+"""
+
+
+def curious_fraction(a: int, b: int) -> bool:
     frac = a / b
     common = ''.join(set(str(a)).intersection(str(b)))
-    print(f"common in {a}/{b}: {common}")
     for l in common:
         cur_a = str(a).replace(l, '')
         cur_b = str(b).replace(l, '')
@@ -10,25 +24,25 @@ def curious_fraction(a: int, b: int):
                 return True
     return False
 
-"""
-# test
-print(curious_fraction(49, 98))
-print(curious_fraction(49, 41))
-print(curious_fraction(30, 50))
-"""
 
-results = []
-for i in range(1, 100):
-    for j in range(1, 100):
-        if i < j:
-            if (i % 10 != 0) and (j % 10 != 0):
-                if curious_fraction(i, j):
-                    results.append([i, j])
-print(results)
+def calc33():
+    results = [[i, j] for i in range(1, 100) for j in range(1, 100) if i < j and (
+        i % 10 != 0) and (j % 10 != 0) and curious_fraction(i, j)]
+    print(results)
 
-num = 1
-denum = 1
-for r in results:
-    num *= r[0]
-    denum *= r[1]
-print(num, '/', denum)
+    num = denum = 1
+    for r in results:
+        num *= r[0]
+        denum *= r[1]
+    print(f"{num} / {denum}")
+
+
+if __name__ == '__main__':
+    testing = False
+
+    if testing:
+        print(curious_fraction(49, 98))
+        print(curious_fraction(49, 41))
+        print(curious_fraction(30, 50))
+    else:
+        calc33()
