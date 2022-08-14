9f6e43eec7c5187e6d932f821656eb5654e2ea51
sevmarc <marton.sevella@gmail.com>
2022-08-14 20:51:56 +0200

    Update problem #35

--- problems/35.py
+++ problems/35.py
@@ -1,49 +1,38 @@
-from math import sqrt
-# circular primes
+""" Circular primes
+The number, 197, is called a circular prime because all 
+rotations of the digits: 197, 971, and 719, are themselves prime.
 
-def circulate(x: int):
+There are thirteen such primes below 100: 
+2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+
+How many circular primes are there below one million?
+"""
+
+from function_collection.main import is_prime
+
+
+def circulate(x: int) -> list[int]:
     w = str(x)
-    circ = []
-    circ.append(x)
-    for i in range(len(w)):
-        cur = w[i:] + w[:i]
-        # print(cur)
-        circ.append(int(cur))
-    return circ
-
-# test
-# circulate(197)
-#circulate(1234)
+    return [int(w[i:] + w[:i]) for i in range(len(w))]
 
 
-def is_prime(x):
-    if x <= 1:
-        return False
-    check = 0
-    for i in range(2, int(sqrt(x) + 1)):
-        if x % i == 0:
-            check += 1
-            if check > 0:
-                return False
-    return True
-
-def check_circ(l):
+def check_circular_if_prime(l: list[int]) -> bool:
     for i in l:
         if not is_prime(i):
             return False
     return True
-testval = 100
-counter = 0
-for i in range(2, testval):
-    if check_circ(circulate(i)):
-        counter += 1
 
-print("counter: ", counter)
 
-maxval = 1000000
-counter = 0
-for i in range(2, maxval):
-    if check_circ(circulate(i)):
-        counter += 1
+def calc35(maxval: int = 1000000) -> int:
+    return len(list(filter(None, [check_circular_if_prime(circulate(i)) for i in range(2, maxval)])))
 
-print("counter: ", counter)
+
+if __name__ == '__main__':
+    testing = False
+
+    if testing:
+        print(circulate(197))
+        print(circulate(1234))
+    else:
+        result = calc35()
+        print(f"{result = }")
