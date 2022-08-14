72413df7a736e7dfd707f166bdf7ae4c8ab51742
sevmarc <marton.sevella@gmail.com>
2022-08-14 21:03:51 +0200

    Update problem #37

--- problems/37.py
+++ problems/37.py
@@ -1,23 +1,28 @@
-from math import sqrt
+""" Truncatable primes
+The number 3797 has an interesting property. Being prime 
+itself, it is possible to continuously remove digits from left 
+to right, and remain prime at each stage: 3797, 797, 97, and 7. 
+Similarly we can work from right to left: 3797, 379, 37, and 3.
 
-def is_prime(x):
-    if x <= 1:
-        return False
-    check = 0
-    for i in range(2, int(sqrt(x) + 1)):
-        if x % i == 0:
-            check += 1
-            if check > 0:
-                return False
-    return True
+Find the sum of the only eleven primes that are both truncatable 
+from left to right and right to left.
 
-def trunc_left(x: int):
+NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+"""
+
+from function_collection.main import is_prime
+from function_collection.main import timer_wrapper
+
+
+def trunc_left(x: int) -> bool:
     w = str(x)
     if len(w) > 1:
         return is_prime(x) and trunc_left(int(w[1:]))
     else:
         return is_prime(x)
-def trunc_right(x: int):
+
+
+def trunc_right(x: int) -> bool:
     w = str(x)
     if len(w) > 1:
         return is_prime(x) and trunc_right(int(w[:len(w)-1]))
@@ -25,21 +30,19 @@ def trunc_right(x: int):
         return is_prime(x)
 
 
-# test
-print(trunc_left(3797))
-print(trunc_right(3797))
-print(trunc_left(7))
+def calc37(maxval: int = 1000000) -> int:
+    result_list = [i for i in range(
+        10, maxval) if trunc_left(i) and trunc_right(i)]
+    return sum(result_list)
 
-maxval = 1000000
-sum = 0
-counter = 0
-max_counter = 11  # we know this
-for i in range(10, maxval):
-    if trunc_left(i) and trunc_right(i):
-        print(i)
-        sum += i
-        counter += 1
-        if counter == max_counter:
-            break
 
-print('SUM: ', sum)
+if __name__ == '__main__':
+    testing = False
+
+    if testing:
+        print(trunc_left(3797))
+        print(trunc_right(3797))
+        print(trunc_left(7))
+    else:
+        result = timer_wrapper(calc37)
+        print(f"{result = }")
