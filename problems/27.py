6d3c28e292c0ad22177b875ed82b6a9a8ea478f1
sevmarc <marton.sevella@gmail.com>
2022-08-14 17:50:31 +0200

    Refactoring to avoid re-implementation, formatting

--- problems/27.py
+++ problems/27.py
@@ -1,48 +1,37 @@
+""" Quadratic primes
+https://projecteuler.net/problem=27
+"""
 from math import sqrt
+from function_collection.main import is_prime
 
-def is_prime(x):
-    if x <= 1:
-        return False
-    check = 0
-    for i in range(2, int(sqrt(x) + 1)):
-        if x % i == 0:
-            check += 1
-            if check > 0:
-                return False
-    return True
 
-"""
-print(is_prime(2))
-print(is_prime(3))
-print(is_prime(4))
-"""
-
-def equa(x, a, b):
+def equa(x: int, a: int, b: int) -> bool:
     cur = x*x + a*x + b
-    # print(cur)
     return is_prime(cur)
 
-def quad(a, b):
+
+def quad(a: int, b: int) -> int:
     x = 0
-    while(equa(x, a, b)):
+    while (equa(x, a, b)):
         x += 1
     return x
 
-# test
-# print(quad(-79, 1601))
 
-constr = 1000
+if __name__ == '__main__':
+    testing = False
 
-max = 0
-max_a = 0
-max_b = 0
+    if testing:
+        print(quad(-79, 1601))
+    else:
+        constr = 1000
+        max_ = max_a = max_b = 0
 
-for a in range(-constr, constr):
-    for b in range(-constr,constr):
-        cur = quad(a,b)
-        if cur > max:
-            max = cur
-            max_a = a
-            max_b = b
-print(max_a, max_b, ': ', max)
-print("Result: ", max_a*max_b)
+        for a in range(-constr, constr):
+            for b in range(-constr, constr):
+                cur = quad(a, b)
+                if cur > max_:
+                    max_ = cur
+                    max_a = a
+                    max_b = b
+        print(f"{max_a = }, {max_b = }: {max_}")
+        print(f"Result: {max_a*max_b}")
