ccce603ae159f6b0b49add109cd39327c27a663d
sevmarc <marton.sevella@gmail.com>
2022-08-16 10:16:10 +0200

    Extend file handling debug

--- problems/function_collection/utils.py
+++ problems/function_collection/utils.py
@@ -4,7 +4,8 @@ import pathlib
 
 def handle_filepath(file_path: str, debug: bool = False) -> str:
     problems_dir_path = pathlib.Path(__file__).parent.resolve().parent.resolve()
+    filename = os.path.join(problems_dir_path, file_path)
     if debug:
         print(f"{problems_dir_path = }")
-    filename = os.path.join(problems_dir_path, file_path)
+        print(f"{filename = }")
     return filename
