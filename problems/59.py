e03fa99c357960fcdca5f618d416fb2b6c3c46bc
sevmarc <marton.sevella@gmail.com>
2022-08-17 08:22:56 +0200

    Update problems #59, #60

--- problems/59.py
+++ problems/59.py
@@ -1,28 +1,59 @@
-# XOR decryption
+""" XOR decryption
+Each character on a computer is assigned a unique code and the 
+preferred standard is ASCII (American Standard Code for Information 
+Interchange). For example, uppercase A = 65, asterisk (*) = 42, and 
+lowercase k = 107.
+
+A modern encryption method is to take a text file, convert the bytes to 
+ASCII, then XOR each byte with a given value, taken from a secret key. 
+The advantage with the XOR function is that using the same encryption 
+key on the cipher text, restores the plain text; for example, 65 XOR 42 
+= 107, then 107 XOR 42 = 65.
+
+For unbreakable encryption, the key is the same length as the plain 
+text message, and the key is made up of random bytes. The user would 
+keep the encrypted message and the encryption key in different 
+locations, and without both "halves", it is impossible to decrypt the 
+message.
+
+Unfortunately, this method is impractical for most users, so the 
+modified method is to use a password as a key. If the password is 
+shorter than the message, which is likely, the key is repeated 
+cyclically throughout the message. The balance for this method is using 
+a sufficiently long password key for security, but short enough to be 
+memorable.
+
+Your task has been made easy, as the encryption key consists of three 
+lower case characters. Using p059_cipher.txt (right click and 'Save 
+Link/Target As...'), a file containing the encrypted ASCII codes, and 
+the knowledge that the plain text must contain common English words, 
+decrypt the message and find the sum of the ASCII values in the 
+original text.
+"""
+
 import time
 import string
 from function_collection.utils import handle_filepath
-
+from function_collection.main import timer_wrapper
 
 # the encryption key consists of three lower case characters
 
-def read_message_from_file(file):
+
+def read_message_from_file(file: str) -> list[str]:
     vals = [line.split(',') for line in open(file, 'r')][0]
     return [chr(int(val)) for val in vals]
 
-def decode(message, key):
-    decoded = []
-    for i,l in enumerate(message):
-        k = key[i % 3]
-        decoded.append(chr(ord(l) ^ ord(k)))
-    return decoded
 
-def check_for_words(word, min_nu_of_occurence, message):
+def decode(message: str, key: str) -> list[str]:
+    return [chr(ord(l) ^ ord(key[i % 3])) for i, l in enumerate(message)]
+
+
+def check_for_words(word: str, min_nu_of_occurence: int, message: str) -> bool:
     count = 0
-    for i,m in enumerate(message):
+    for i, m in enumerate(message):
         notfound = True
         if m == word[0]:
-            for j,l in enumerate(word):
+            for j, l in enumerate(word):
                 if message[i + j] != l:
                     notfound = False
             if notfound:
@@ -32,7 +63,8 @@ def check_for_words(word, min_nu_of_occurence, message):
     else:
         return False
 
-def all_keys():
+
+def all_keys() -> list[str]:
     keys = []
     for l0 in list(string.ascii_lowercase):
         for l1 in list(string.ascii_lowercase):
@@ -42,21 +74,26 @@ def all_keys():
     return keys
 
 
-start_time = time.time()
-
-msg = read_message_from_file(handle_filepath('inputfiles/59.txt'))
-keylist = all_keys()
-# print(msg)
-# print(check_for_words('the', 2, "the man and the cow"))
-for k in keylist:
-    d = decode(msg, k)
-    if check_for_words('the', 7, d):
-        w = ''
-        sum = 0
-        for l in d:
-            sum += ord(l)
-            w += l
-        print(w, k, sum)
+def calc59(msg: str, keylist: list[str]) -> tuple[str, str, int]:
+    for k in keylist:
+        d = decode(msg, k)
+        if check_for_words('the', 7, d):
+            w = ''.join([l for l in d])
+            sum_ = sum([ord(l) for l in d])
+            return (w, k, sum_)
 
 
-print(time.time() - start_time)
+if __name__ == '__main__':
+    msg = read_message_from_file(handle_filepath('inputfiles/59.txt'))
+    keylist = all_keys()
+
+    testing = False
+
+    if testing:
+        print(f"{msg = }")
+        print(check_for_words('the', 2, "the man and the cow"))
+    else:
+        decoded_message, key_, sum_ = timer_wrapper(calc59, [msg, keylist])
+        print(f"{decoded_message = }")
+        print(f"{key_ = }")
+        print(f"{sum_ = }")
--- problems/60.py
+++ problems/60.py
@@ -1,33 +1,36 @@
-from itertools import chain, combinations
-from math import sqrt
-import time
+""" Prime pair sets
+The primes 3, 7, 109, and 673, are quite remarkable. By taking 
+any two primes and concatenating them in any order the result 
+will always be prime. For example, taking 7 and 109, both 7109 
+and 1097 are prime. The sum of these four primes, 792, represents 
+the lowest sum for a set of four primes with this property.
 
-def powerset(iterable):
+Find the lowest sum for a set of five primes for which any two 
+primes concatenate to produce another prime.
+"""
+
+from itertools import chain, combinations
+from typing import Iterable, Iterator
+from function_collection.main import is_prime, generate_primes
+from function_collection.main import timer_wrapper
+
+
+def powerset(iterable: Iterable) -> Iterator:
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-def is_prime(x):
-    if x <= 1:
-        return False
-    for i in range(2, int(sqrt(x) + 1)):
-        if x % i == 0:
-            return False
-    return True
 
-def generate_primes(x):
-    return [p for p in range(x) if is_prime(p)]
-
-def create_subset(l, n):
+def create_subset(l: list[int], n: int) -> list[chain[tuple[int]]]:
     return [i for i in powerset(l) if len(i) == n]
 
-def concat_primes(ab):
-    a = ab[0]
-    b = ab[1]
+
+def concat_primes(ab: list[int]) -> bool:
+    a, b = ab
     return is_prime(int(str(a) + str(b))) and is_prime(int(str(b) + str(a)))
 
 
-def lookup():
+def lookup() -> None:
     allprimes = generate_primes(1000)
     combo_5_primes = create_subset(allprimes, 4)
     for options in combo_5_primes:
@@ -39,23 +42,22 @@ def lookup():
             print("FOUND: ", every2)
             break
 
-def lookup2():
+
+def lookup2() -> None:
     allprimes = generate_primes(10000)
     for p1 in allprimes:
         for p2 in allprimes:
-            if concat_primes([p1,p2]):
+            if concat_primes([p1, p2]):
                 for p3 in allprimes:
-                    if concat_primes([p1,p3]) and concat_primes([p2, p3]):
+                    if concat_primes([p1, p3]) and concat_primes([p2, p3]):
                         for p4 in allprimes:
-                            if concat_primes([p1,p4]) and concat_primes([p2, p4]) and concat_primes([p3,p4]):
+                            if concat_primes([p1, p4]) and concat_primes([p2, p4]) and concat_primes([p3, p4]):
                                 for p5 in allprimes:
-                                    if concat_primes([p1,p5]) and concat_primes([p2, p5]) and concat_primes([p3,p5]) and concat_primes([p4, p5]):
+                                    if concat_primes([p1, p5]) and concat_primes([p2, p5]) and concat_primes([p3, p5]) and concat_primes([p4, p5]):
                                         print("FOUND: ", p1, p2, p3, p4, p5)
                                         print("SUM", p1+p2+p3+p4+p5)
                                         return
 
-start_time = time.time()
 
-lookup2()
-
-print(time.time() - start_time)
+if __name__ == '__main__':
+    timer_wrapper(lookup2)
