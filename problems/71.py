ead3ca169fe8018093d919ac4bd5749afe8f7d9f
sevmarc <marton.sevella@gmail.com>
2022-08-19 17:25:08 +0200

    Updated problem #71

--- problems/71.py
+++ problems/71.py
@@ -1,4 +1,4 @@
-"""
+""" Ordered fractions
 Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
 
 If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
@@ -10,22 +10,25 @@ It can be seen that 2/5 is the fraction immediately to the left of 3/7.
 By listing the set of reduced proper fractions for d ≤ 1,000,000 in ascending order of size, find the numerator of the fraction immediately to the left of 3/7.
 """
 
-import time
+from function_collection.main import timer_wrapper
 from math import gcd as bltin_gcd
 
-goal = 3 / 7
-maximum = 1000000
-epsilon = 100
 
-start_time = time.time()
+def calc71() -> int:
+    goal = 3 / 7
+    maximum = 1000000
+    epsilon = 100
 
-while(maximum > 10):
-    current = int(maximum * goal)
-    if (bltin_gcd(current, maximum) == 1):
-        if abs((current/maximum) - goal) < epsilon:
-            epsilon = abs((current/maximum) - goal)
-            numerator = current
-    maximum -= 1
-print("Result: ", numerator)
+    while (maximum > 10):
+        current = int(maximum * goal)
+        if (bltin_gcd(current, maximum) == 1):
+            if abs((current/maximum) - goal) < epsilon:
+                epsilon = abs((current/maximum) - goal)
+                numerator = current
+        maximum -= 1
+    return numerator
 
-print(time.time() - start_time)
+
+if __name__ == '__main__':
+    result = timer_wrapper(calc71)
+    print(f"{result = }")
