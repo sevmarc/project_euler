1d43609d90d6f771e5908be1e47ffac176437fc0
sevmarc <marton.sevella@gmail.com>
2022-08-19 17:49:47 +0200

    Updated problem #72

--- problems/72.py
+++ problems/72.py
@@ -16,48 +16,37 @@ fractions for d ≤ 1,000,000?
 
 from function_collection.main import unique_prime_divisors, timer_wrapper
 from math import gcd as bltin_gcd
-from math import sqrt
 from sympy import totient
 
 
 def hcf_1(d: int) -> int:
     """ Returns the number of numbers that have a HCF 
     of 1 with the input 'd' number """
-    count = 0
-    for i in range(1, d):
-        if bltin_gcd(i, d) == 1:
-            # print("\t", i, d)
-            count += 1
-    return count
+    return len([i for i in range(1, d) if bltin_gcd(i, d) == 1])
+
 
 def calc_rec(lim_: int) -> int:
     """ Sums hcf_1s recursively """
     if lim_ == 1:
         return 0
     return hcf_1(lim_) + calc_rec(lim_-1)
-    
+
+
 def calc_lin(lim_: int) -> int:
     """ Sums hcf_1s linearly """
-    sum_ = 0
-    for i in range(lim_ + 1):
-        sum_ += hcf_1(i)
-    return sum_
+    return sum([hcf_1(i) for i in range(lim_ + 1)])
+
 
 def calc_tot(lim_: int) -> int:
     """ Sums hcf_1s using Euler's totient function (from sympy) """
-    sum_ = 0
-    for i in range(2, lim_ + 1):
-        sum_ += totient(i)
-    return sum_
+    return sum([totient(i) for i in range(2, lim_ + 1)])
 
-
-testing = False
 
 if __name__ == '__main__':
+    testing = False
     if testing:
-        for i in range(2,26):
+        for i in range(2, 26):
             print(i, hcf_1(i))
     else:
         val = int(1e6)
         print(timer_wrapper(calc_tot, val))
-
