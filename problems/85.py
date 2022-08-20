9c7f83ffede63f47fbca75e2af08476d7f8b924e
sevmarc <marton.sevella@gmail.com>
2022-08-20 13:35:02 +0200

    Update problem #85, optimization

--- problems/85.py
+++ problems/85.py
@@ -1,8 +1,12 @@
-"""
-By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles:
+""" Counting rectangles
+By counting carefully it can be seen that a rectangular grid measuring 3 
+by 2 contains eighteen rectangles:
 
-Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with the nearest solution.
+Although there exists no rectangular grid that contains exactly two million 
+rectangles, find the area of the grid with the nearest solution.
 """
+
+from function_collection.main import timer_wrapper
 
 """
 1x1 -> 1  (1)
@@ -13,40 +17,41 @@ Although there exists no rectangular grid that contains exactly two million rect
 3x3 -> 36 (9 + 6 + 3 + 6 + 4 + 2 + 3 + 2 + 1)
 """
 
-from function_collection.main import timer_wrapper
 
-
-def calc_rects(sides, acc=0):
+def calc_rects(sides: list[int], acc: int = 0) -> int:
     x, y = sides
     curr = acc
     if y >= 1:
         curr += sum(range(x+1)) * y
         return calc_rects([x, y-1], curr)
     else:
-        # print(curr)
         return curr
 
-def calc85():
-    est = 2000000
+
+def calc85(debug: bool = False) -> int:
+    est = 2e6
     minimum = 10000
-    notfound = True
     for i in range(100):
         for j in range(i):
-            print(i, j)
-            current = calc_rects([i,j])
+            current = calc_rects([i, j])
             if abs(current - est) < minimum:
                 minimum = abs(current - est)
                 place = [i, j]
-            if current > est + 1000:
-                notfound = False
-    print(place, calc_rects(place), '; area: ', place[0] * place[1])
+    if debug:
+        print(place, calc_rects(place), '; area: ', place[0] * place[1])
+    return place[0] * place[1]
 
-"""
-timer_wrapper(calc_rects, [1,1])
-timer_wrapper(calc_rects, [2,1])
-timer_wrapper(calc_rects, [2,2])
-timer_wrapper(calc_rects, [3,1])
-timer_wrapper(calc_rects, [3,2])
-timer_wrapper(calc_rects, [3,3])
-"""
-timer_wrapper(calc85)
+
+if __name__ == '__main__':
+    testing = False
+
+    if testing:
+        print(timer_wrapper(calc_rects, [1, 1]))
+        print(timer_wrapper(calc_rects, [2, 1]))
+        print(timer_wrapper(calc_rects, [2, 2]))
+        print(timer_wrapper(calc_rects, [3, 1]))
+        print(timer_wrapper(calc_rects, [3, 2]))
+        print(timer_wrapper(calc_rects, [3, 3]))
+    else:
+        result = timer_wrapper(calc85)
+        print(f"{result = }")
