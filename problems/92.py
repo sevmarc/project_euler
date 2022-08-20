1390d6b494636e00b37220ce52ef0e94674638c7
sevmarc <marton.sevella@gmail.com>
2022-08-20 14:13:04 +0200

    Update problem #92

--- problems/92.py
+++ problems/92.py
@@ -1,33 +1,40 @@
-import collections
-import time
+""" Square digit chain
+A number chain is created by continuously adding the square of the 
+digits in a number to form a new number until it has been seen before.
 
-def square_digits(n:int):
+For example,
+
+44 → 32 → 13 → 10 → 1 → 1
+85 → 89 → 145 → 42 → 20 → 4 → 16 → 37 → 58 → 89
+
+Therefore any chain that arrives at 1 or 89 will become stuck in an 
+endless loop. What is most amazing is that EVERY starting number will 
+eventually arrive at 1 or 89.
+
+How many starting numbers below ten million will arrive at 89?
+"""
+
+from function_collection.main import timer_wrapper
+
+
+def square_digits(n: int) -> int:
     lst = [(int(i))**2 for i in str(n)]
     return sum(lst)
 
-def iterate(init:int):
-    while(1):
+
+def iterate(init: int) -> int:
+    while (1):
         init = square_digits(init)
         if init == 1 or init == 89:
             return init
 
-def counter(limit):
-    count = 0
-    pairs = {}
-    for i in range(1, limit):
-        print(i, count)
-        if i in pairs:
-            it = pairs[i]
-        else:
-            it = iterate(i)
-            pairs.update({i: it})
-        if it == 89:
-            count += 1
-    print(count)
 
-start_time = time.time()
+def counter(limit: int) -> int:
+    pairs = {i: iterate(i) for i in range(1, limit)}
+    return len([i for i in list(pairs.values()) if i == 89])
 
-counter(10000000)
-# can be greatly optimized
 
-print(time.time() - start_time)
+if __name__ == '__main__':
+    result = timer_wrapper(counter, 10000000)
+    print(f"{result = }")
+    # can be greatly optimized
