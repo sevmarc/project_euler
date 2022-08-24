a7c97d784c977ae61327ee2353340e3543a14bf7
sevmarc <marton.sevella@gmail.com>
2022-08-24 07:53:48 +0200

    current algorithm does not work properly

--- problems/76_todo.py
+++ problems/76_todo.py
@@ -1,4 +1,18 @@
-"""
+""" Counting summations
+It is possible to write five as a sum in exactly six different ways:
+
+4 + 1
+3 + 2
+3 + 1 + 1
+2 + 2 + 1
+2 + 1 + 1 + 1
+1 + 1 + 1 + 1 + 1
+
+How many different ways can one hundred be written as a sum of at least two 
+positive integers?
+--------------------------------------------------------------------
+
+
 f(x) = 2_sum + f(x-1)
 7 = 6 + 1 ; 5 + 2; 4 + 3; (3 + 4...)                    3
 8 = 7 + 1 ; 6 + 2; 5 + 3; 4 + 4 ; (3 + 5...)            4
@@ -45,14 +59,22 @@ f(x) = 2_sum + f(x-1)
 -----
 1 + 1
 """
+
 from math import floor
+from function_collection.main import timer_wrapper
+
 
 def rec(a):
     if a == 2:
         return 1
     return floor(a / 2) + rec(a-1)
 
-n = 101
-for i in range(2,n + 1):
-    print(i, rec(i))
-# 190569291
+
+def calc76(n: int = 100) -> int:
+    possibilities = sum([rec(i) for i in range(2, n + 1)])
+    return possibilities
+
+
+if __name__ == '__main__':
+    result = timer_wrapper(calc76)
+    print(f"{result = }")  # 190569291
