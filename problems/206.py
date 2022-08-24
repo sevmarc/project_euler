914d5d179e463a8b6b0391e4d385e33ccc4e58cc
sevmarc <marton.sevella@gmail.com>
2022-08-24 16:40:21 +0200

    #206 updated

--- problems/206.py
+++ problems/206.py
@@ -1,6 +1,6 @@
 """ Concealed Square
-Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
-where each “_” is a single digit.
+Find the unique positive integer whose square has the form 
+1_2_3_4_5_6_7_8_9_0, where each “_” is a single digit.
 """
 
 from math import sqrt
@@ -12,17 +12,17 @@ def check_pattern(n: int) -> bool:
     nword = str(n)
     # 1_2_3_4_5_6_7_8_9_0
     return nword[0] == '1' and \
-            nword[2] == '2' and \
-            nword[4] == '3' and \
-            nword[6] == '4' and \
-            nword[8] == '5' and \
-            nword[10] == '6' and \
-            nword[12] == '7' and \
-            nword[14] == '8' and \
-            nword[16] == '9'
+        nword[2] == '2' and \
+        nword[4] == '3' and \
+        nword[6] == '4' and \
+        nword[8] == '5' and \
+        nword[10] == '6' and \
+        nword[12] == '7' and \
+        nword[14] == '8' and \
+        nword[16] == '9'
 
 
-def solution():
+def solution() -> int:
     """
     last digit must be 0 <- roots last digit must also be 0
     can the second to last digit of root be not 0?
@@ -37,14 +37,16 @@ def solution():
     end_root = int(sqrt(max_)) + 1
     # root cant have 00 ending
     # then square would have 0000 ending
-    
+
     for i in range(init_root, end_root, 10):
         if str(i)[-2:] == '30' or \
            str(i)[-2:] == '70':
             sq_ = i * i
             if check_pattern(sq_):
-                print(i, sq_)
+                return i
+    return -1
+
 
 if __name__ == '__main__':
-    # guess()
-    timer_wrapper(solution)
+    result = timer_wrapper(solution)
+    print(f"{result = }")
