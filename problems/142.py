922839c5e95da3f89c479bf023ad0f3a6a095fe2
sevmarc <marton.sevella@gmail.com>
2022-08-24 16:36:26 +0200

    #142 solution updated

--- problems/142.py
+++ problems/142.py
@@ -1,6 +1,10 @@
 """ Perfect Square Collection
-Find the smallest x + y + z with integers x > y > z > 0 such that x + y, x − y, x + z, x − z, y + z, y − z are all perfect squares.
+Find the smallest x + y + z with integers x > y > z > 0 such that 
+x + y, x - y, x + z, x - z, y + z, y - z are all perfect squares.
 """
+
+from function_collection.main import timer_wrapper, is_square
+from math import sqrt
 
 """
 x>y>z>0
@@ -16,11 +20,8 @@ y+z -----------------------
 y-z ---
 """
 
-from function_collection.main import timer_wrapper, is_square
-from math import sqrt
 
-
-def generate_xyz_upto_x(x: int) -> list:
+def generate_xyz_upto_x(x: int) -> list[int]:
     starter = 1
     xyz_list = []
     for i in range(starter, x):
@@ -28,8 +29,9 @@ def generate_xyz_upto_x(x: int) -> list:
             xyz_list.append([x, i, j])
     return xyz_list
 
-def check_xyz_squares(xyz: list) -> bool:
-    x,y,z = xyz
+
+def check_xyz_squares(xyz: list[int]) -> bool:
+    x, y, z = xyz
     to_check = [x + y,
                 x - y,
                 x + z,
@@ -41,7 +43,8 @@ def check_xyz_squares(xyz: list) -> bool:
             return False
     return True
 
-def naive_solution():
+
+def naive_solution() -> list[int]:
     found = False
     iter_ = 1
     while not found:
@@ -51,8 +54,10 @@ def naive_solution():
                 found = True
                 print("Found: ", xyz)
         iter_ += 1
+    return xyz
 
-def create_yz_pairs(yzs: list):
+
+def create_yz_pairs(yzs: list) -> list[list[int]]:
     """
     From a list of numbers, creates all distinct pairs.
     """
@@ -62,19 +67,18 @@ def create_yz_pairs(yzs: list):
         yzs_removing.remove(yz)
         for yz_ in yzs_removing:
             combinations.append([yz, yz_])
-    # print(yzs, combinations)
     return combinations
 
 
-def check_yz(yz: list) -> bool:
+def check_yz(yz: list[int]) -> bool:
     """
     Checks a 'y' - 'z' pair, whether y+z and y-z are squares
     """
-    y,z = max(yz), min(yz)
+    y, z = max(yz), min(yz)
     return is_square(y-z) and is_square(y+z)
 
 
-def solution():
+def solution() -> int:
     """
     X is equidistant from two sets of squares
     <---<---x--->--->
@@ -92,7 +96,7 @@ def solution():
     found = False  # condition for while statement
     while not found:
         # 1- generate list of squares up to x
-        sq_list = [i*i for i in range(1, int(sqrt(x)))] 
+        sq_list = [i*i for i in range(1, int(sqrt(x)))]
         equi = []  # list to store equidistant squares
         # 2. check for equidistant squares
         for sq in sq_list:
@@ -103,10 +107,13 @@ def solution():
         for yz in create_yz_pairs(equi):
             if check_yz(yz):
                 found = True
-                y,z = yz[0], yz[1]
-                print(f"x: {x}, y: {y}, z: {z}, sum: {x+y+z}, squares: {sqrt(x-y)}, {sqrt(x+y)}, {sqrt(x-z)}, {sqrt(x+z)}, {sqrt(y-z)}, {sqrt(y+z)}")
+                y, z = yz[0], yz[1]
+                print(
+                    f"x: {x}, y: {y}, z: {z}, sum: {x+y+z}, squares: {sqrt(x-y)}, {sqrt(x+y)}, {sqrt(x-z)}, {sqrt(x+z)}, {sqrt(y-z)}, {sqrt(y+z)}")
         x += 1  # iterate
-    return x + y + z  #  sum in question
+    return x + y + z  # sum in question
 
 
-print(timer_wrapper(solution))
+if __name__ == '__main__':
+    result = timer_wrapper(solution)
+    print(f"{result = }")
